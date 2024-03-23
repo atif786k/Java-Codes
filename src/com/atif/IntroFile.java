0d61e11d9b08f35ac092b18c183671fe5a36ed9c
Mohammad Atif <matif91201@gmail.com>
2024-03-24 00:14:14 +0530

    Intro file added

---
+++ src/com/atif/IntroFile.java
@@ -0,0 +1,10 @@
+package com.atif;
+
+public class IntroFile {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Hey Guys, this is Mohammad Atif here");
+        System.out.println("These are my leetcode problems and basic practice of mine to enhance my problem solving skills");
+    }
+}
