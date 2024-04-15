3ba808bc6f679913b1b68877f603ace588a889e0
Mohammad Atif <matif91201@gmail.com>
2024-04-15 17:22:54 +0530

    15/04

--- src/com/atif/Searching.java
+++ src/com/atif/Searching.java
@@ -39,7 +39,7 @@ public class Searching {
         int end = arr.length - 1;
 
         while (start <= end) {
-            int mid = start + (end - start)/ 2;
+            int mid = start + (end - start) / 2;
 
             if (arr[mid] > target) {
                 end = mid - 1;
@@ -53,22 +53,51 @@ public class Searching {
         return start;
     }
 
-    static int missingNumber(int[] arr){
-        return 0;
+    //    Finding the missing number in the range of the length of given array:--
+    static int missingNumber(int[] arr) {
+        int len = arr.length;
+        Arrays.sort(arr);
+        int missNum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (missNum == arr[i]) {
+                missNum += 1;
+            }
+        }
+        return missNum;
+    }
+
+    public char findingChar(char[] letters, char target) {
+        int start = 0;
+        int end = letters.length - 1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (letters[mid] > target) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return letters[start % letters.length];
     }
 
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int[] nums = {1, 2, 3, 4, 5, 21, 32, 45, 90};
+        Searching sr = new Searching();
+        int[] nums = {3, 0, 4, 2, 1};
 
 //        Q-Search Insert Position:-
 //        int ans=searchInsetPosition(nums, 10);
 //        System.out.println(ans);
 
 //        Q-Missing Number:-
-        int ans=missingNumber(nums);
-        System.out.println(ans);
+//        int ans = missingNumber(nums);
+//        System.out.println(ans);
 
+//        Q-Find smallest letter greater than target:-
+        char[] letters = {'c', 'f', 'j'};
+        char target = 'a';
+        int ans = sr.findingChar(letters, target);
+        System.out.println(ans);
     }
 }
