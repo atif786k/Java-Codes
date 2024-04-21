3ded7cb1d2bda602d61c3c8733fdf6b09e55b4d4
Mohammad Atif <matif91201@gmail.com>
2024-04-21 22:42:49 +0530

    commit 21/04

--- src/com/atif/Searching.java
+++ src/com/atif/Searching.java
@@ -2,20 +2,9 @@ package com.atif;
 
 import java.util.Arrays;
 import java.util.Scanner;
-//Sorting Questions :
+//Searching ( Linear and Binary ) Questions :
 
 public class Searching {
-    static int min(int[] arr) {
-        int ans = arr[0];
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] < ans) {
-                ans = arr[i];
-            }
-        }
-        return ans;
-
-    }
-
     static int binarySearch(int[] arr, int target) {
         int start = 0;
         int end = arr.length - 1;
@@ -32,6 +21,18 @@ public class Searching {
             }
         }
         return -1;
+    }
+
+
+    static int min(int[] arr) {
+        int ans = arr[0];
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] < ans) {
+                ans = arr[i];
+            }
+        }
+        return ans;
+
     }
 
     static int searchInsetPosition(int[] arr, int target) {
@@ -113,11 +114,95 @@ public class Searching {
         return indicies;
     }
 
+    public int peakIndexMountainArray(int[] arr) {
+        int s = 0;
+        int e = arr.length - 1;
+
+//        First approach :-
+//        while (s <= e) {
+//            if (arr[s] > arr[e] && arr.length >= 3) {
+//                e--;
+//            } else {
+//                s++;
+//            }
+//        }
+//        return e;
+
+        while (s < e) {
+            int mid = s + (e - s) / 2;
+            if (arr[mid] > arr[mid + 1]) {
+                e = mid;
+            } else {
+                s = mid + 1;
+            }
+        }
+        return s;
+    }
+
+//    Q-Find the first bad version :-
+//    public int firstBadVersion(int n) {
+//        int s = 1;
+//        int e = n;
+//        int ans = 0;
+//        while(s <= e){
+//            int mid = s + (e - s) / 2;
+//            boolean x = isBadVersion(mid);
+//            if(x == true){
+//                ans = mid;
+//                e = mid - 1;
+//            } else{
+//                s = mid + 1;
+//            }
+//        }
+//        return ans;
+//    }
+
+
+    public boolean canJump(int[] arr) {
+        int i = 0;
+        while (i < arr.length) {
+            int jumpNum = arr[i];
+            if (i < arr.length) {
+                i += jumpNum;
+            } else if (i == arr.length - 1) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean checkIfExists(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length; j++) {
+                if (arr[i] == 2 * arr[j] && i != j) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    static int singleNonDuplicate(int[] nums) {
+        int s = 0;
+        int e = nums.length - 1;
+        while (s < e) {
+            int mid = s + (e - s) / 2;
+            if (mid % 2 == 1) {
+                mid--;
+            }
+            if (nums[mid] == nums[mid + 1]) {
+                s = mid + 2;
+            } else {
+                e = mid;
+            }
+        }
+        return nums[s];
+    }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         Searching sr = new Searching();
-        int[] nums = {3, 0, 4, 2, 1};
+//        int[] nums = {3, 0, 4, 2, 1};
 
 //        Q-Search Insert Position:-
 //        int ans=searchInsetPosition(nums, 10);
@@ -134,9 +219,78 @@ public class Searching {
 //        System.out.println(ans);
 
 //        Q-Two Sum II:-
-        int[] numbers = {2, 3, 4};
-        int target = 6;
-        System.out.println(Arrays.toString(sr.twoSumII(numbers, target)));
+//        int[] numbers = {2, 3, 4};
+//        int target = 6;
+//        System.out.println(Arrays.toString(sr.twoSumII(numbers, target)));
 
+//        Q-Peak Index in Mountain Array :-
+//        int[] arr = {0, 10, 1, 2, 3, 4, 50};
+//        System.out.println(sr.peakIndexMountainArray(arr));
+
+//        Q-Jump game :- ( not solved )
+//        int[] arr = {3, 2, 1, 0, 4};
+//        System.out.println(sr.canJump(arr));
+
+//        Q-check if N and it's double exists or not :-
+//        int[] arr = {10, 2, 5, 3};
+//        System.out.println(sr.checkIfExists(arr));
+
+//        Q-Search non-duplicate number in the given array :-
+//        int[] arr = {1, 1, 2, 3, 3, 4, 4, 8, 8};
+//        System.out.println(singleNonDuplicate(arr));
+
+//        Q-Searching in rotated sorted array :-
+        int[] arr = {3,1};
+        int target = 3;
+        int ans = searchingUsingPivot(arr, target);
+        System.out.println(ans);
+
+    }
+
+//    Searching in rotated sorted array :-
+    static int findPivot(int[] arr) {
+        int start = 0;
+        int end = arr.length - 1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (mid < end && arr[mid] > arr[mid + 1]) {
+                return mid;
+            } else if (mid > start && arr[mid] < arr[mid - 1]) {
+                return mid - 1;
+            } else if (arr[mid] < arr[start]) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return -1;
+    }
+
+    static int binarySearchUsingPivot(int start, int end, int[] arr, int target) {
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (arr[mid] > target) {
+                end = mid - 1;
+            } else if (arr[mid] < target) {
+                start = mid + 1;
+            } else if (arr[mid] == target) {
+                return mid;
+            }
+        }
+        return -1;
+    }
+
+    static int searchingUsingPivot(int[] arr, int target){
+        int pivot = findPivot(arr);
+        if (pivot == -1){
+            return binarySearchUsingPivot(0, arr.length - 1, arr, target);
+        }
+        if (arr[pivot] == target){
+            return pivot;
+        }
+        if (target >= arr[0]){
+            return binarySearchUsingPivot(0, pivot - 1, arr, target);
+        }
+        return binarySearchUsingPivot(pivot + 1, arr.length - 1, arr, target);
     }
 }
