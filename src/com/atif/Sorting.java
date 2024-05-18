d86de7a442151d0679a5b30f4220ff081baccf33
Mohammad Atif <matif91201@gmail.com>
2024-05-18 23:24:14 +0530

    commit 18/05

--- src/com/atif/Sorting.java
+++ src/com/atif/Sorting.java
@@ -1,6 +1,8 @@
 package com.atif;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class Sorting {
 
@@ -40,6 +42,76 @@ public class Sorting {
         }
     }
 
+    static void insertionSort(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = i + 1; j > 0; j--) {
+                if (arr[j] < arr[j - 1]) {
+                    int temp = arr[j - 1];
+                    arr[j - 1] = arr[j];
+                    arr[j] = temp;
+                } else {
+                    break;
+                }
+            }
+        }
+    }
+
+    //    Only for continuous numbers for range 1 to N
+    static void cyclicSortI(int[] arr) {
+        int i = 0;
+        while (i < arr.length) {
+            int current = arr[i] - 1;
+            if (arr[i] != arr[current]) {
+                int temp = arr[i];
+                arr[i] = arr[current];
+                arr[current] = temp;
+            } else {
+                i++;
+            }
+        }
+    }
+
+    //    Cyclic Sort whose range is 0 to N
+    static void cyclicSortII(int[] arr) {
+        int n = arr.length;
+        int i = 0;
+        while (i < arr.length) {
+            int current = arr[i];
+            if (arr[i] == n) {
+                i++;
+            } else if (arr[i] != arr[current]) {
+                int temp = arr[i];
+                arr[i] = arr[current];
+                arr[current] = temp;
+            } else {
+                i++;
+            }
+        }
+    }
+
+    static int findMissingNum(int[] arr) {
+        int store = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] != i) {
+                store = i;
+                break;
+            } else {
+                store = arr.length;
+            }
+        }
+        return store;
+    }
+
+    static List<Integer> findAllMissingNum(int[] arr) {
+        ArrayList<Integer> res = new ArrayList<Integer>();
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] != i + 1) {
+                res.add(i + 1);
+            }
+        }
+        return res;
+    }
+
     static int maxElementIndex(int[] arr, int start, int end) {
         int maxIndex = start;
         for (int i = 0; i <= end; i++) {
@@ -59,16 +131,43 @@ public class Sorting {
         return false;
     }
 
+    static int findDuplicate(int[] arr) {
+        int store = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] != i + 1) {
+                store = arr[i];
+            }
+        }
+        return store;
+    }
+
+    //    Q-Set Mismatch :-
+    static int[] findErrorNum(int[] arr) {
+        int[] arr1 = new int[2];
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] != i + 1) {
+                arr1[0] = arr[i];
+            }
+            if (arr[i] != i + 1) {
+                arr1[1] = i + 1;
+            }
+        }
+        return arr1;
+    }
+
+    static int maximumProduct(int[] arr) {
+        int i = arr.length - 1;
+        int j = arr.length - 2;
+        int result = (arr[i] - 1) * (arr[j] - 1);
+        return result;
+    }
+
     public static void main(String[] args) {
-        int[] arr = {2, 3, 4, 3, 4, -1, 10, -20, -100};
-//        System.out.println(Arrays.toString(linearSorting(arr)));
+        int[] arr = {3,4,5,2};
 
-//        bubbleSort(arr);
-//        System.out.println(Arrays.toString(arr));
-//        System.out.println(containDuplicate(arr));
-
-        selectionSort(arr);
+        insertionSort(arr);
         System.out.println(Arrays.toString(arr));
+        System.out.println(maximumProduct(arr));
 
     }
 }
